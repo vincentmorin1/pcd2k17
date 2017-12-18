5167fd4f49f72c2decdf6f0f990ce22dd56f9bd7
Vincent Morin <vincent.morin@telecomnancy.eu>
2017-12-18 11:05:34 +0100

    ajout sample

---
+++ src/test/java/eu/telecomnancy/pcd2k17/SampleTest.java
@@ -0,0 +1,13 @@
+package eu.telecomnancy.pcd2k17;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SampleTest {
+
+  @Test
+  public void testDummy() {
+    assertEquals("dummy assertion", 1, 1);
+  }
+
+}
