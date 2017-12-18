2befd8623d4ef0b0df6c9eeb813b9f0a6daffaf9
Vincent Morin <vincent.morin@telecomnancy.eu>
2017-12-18 16:47:27 +0100

    modif interface connexion

--- src/main/java/eu/telecomnancy/pcd2k17/MainController.java
+++ src/main/java/eu/telecomnancy/pcd2k17/MainController.java
@@ -1,48 +1,30 @@
 package eu.telecomnancy.pcd2k17;
 
-import java.awt.TextField;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
-public class MainController {
+public class MainController{
 
   final static Logger log = LogManager.getLogger(MainController.class);
 
-  @FXML
-  public void handleClickOk(ActionEvent event) {
-    log.debug("ok button was clicked!");
-  }
-  
+
   @FXML
   TextField identifiant = new TextField();
-  TextField mdp = new TextField();
-  TextField connexion = new TextField();
-  Text actiontarget = new Text();	
-  
-  public TextField getId() {
-  	return this.identifiant;
-  }
- 
-  public TextField getPwd() {
-	  return this.mdp;
-  }
-  
-  public void setId(TextField identifiant) {
-	  this.identifiant = identifiant;
-  }
-  
-  public void setPwd(TextField mdp) {
-	  this.mdp = mdp;
-  }
-  
   @FXML
-  public void ConnexionPressed (ActionEvent event) {
-	  actiontarget.setText(identifiant.getText());
+  TextField mdp = new TextField();
+  @FXML
+  Button connexion = new Button();
+  
+
+  @FXML
+  public void handleClickOk(ActionEvent event) {
+    log.debug(identifiant.getText());
+	log.debug(mdp.getText());
   }
   
   @FXML
