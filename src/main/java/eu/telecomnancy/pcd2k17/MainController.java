a0589a9fa1f24eace8f3fd665a55db75b00c6f40
Vincent Morin <vincent.morin@telecomnancy.eu>
2017-12-18 14:56:53 +0100

    ajout interface connexion

--- src/main/java/eu/telecomnancy/pcd2k17/MainController.java
+++ src/main/java/eu/telecomnancy/pcd2k17/MainController.java
@@ -1,10 +1,13 @@
 package eu.telecomnancy.pcd2k17;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
+import java.awt.TextField;
 
-import javafx.fxml.FXML;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
 
 public class MainController {
 
@@ -14,5 +17,38 @@ public class MainController {
   public void handleClickOk(ActionEvent event) {
     log.debug("ok button was clicked!");
   }
+  
+  @FXML
+  TextField identifiant = new TextField();
+  TextField mdp = new TextField();
+  TextField connexion = new TextField();
+  Text actiontarget = new Text();	
+  
+  public TextField getId() {
+  	return this.identifiant;
+  }
+ 
+  public TextField getPwd() {
+	  return this.mdp;
+  }
+  
+  public void setId(TextField identifiant) {
+	  this.identifiant = identifiant;
+  }
+  
+  public void setPwd(TextField mdp) {
+	  this.mdp = mdp;
+  }
+  
+  @FXML
+  public void ConnexionPressed (ActionEvent event) {
+	  actiontarget.setText(identifiant.getText());
+  }
+  
+  @FXML
+  public void initialize() {
+	  
+  }
+  
 
 }
