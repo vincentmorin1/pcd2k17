9dfa87cf609c8d931ba0757530088243e042708f
Victor Schwien <schwien1u@tncy-04-12.telecomnancy.site.univ-lorraine.fr>
2017-12-18 15:49:24 +0100

    modif auth (sans login + access token)

--- src/main/java/Authentification/auth.java
+++ src/main/java/Authentification/auth.java
@@ -2,14 +2,15 @@ package Authentification;
 
 import org.gitlab4j.api.GitLabApi;
 
-public class auth {
+public class auth{
 	//Retrouve les identifiants des utilisateurs (en attente de comment reprendre les vrai input de l'interface)
-		String identifiant = "professeur";
-		String password = "PCDpotes" ;
+		static String identifiant = "professeur";
+		static String password = "PCDpotes" ;
+		static String accessToken = "hwVACnEWhZMJ_Zeoz7NS";
 	
 	// Créer une instance pour communiquer avec notre serveur Gitlab
 	//GitLabApi gitLabApi = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr",identifiant) ;
 	
 	// Pour le test d'après
-	GitLabApi gitLabApi = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr", identifiant, password);	
+	public static GitLabApi auth = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr", accessToken);	
 }
