1b66e4c1beccd9d1e937022bb04f562b44062c67
Paul Leroux <leroux46u@tncy-04-13.telecomnancy.site.univ-lorraine.fr>
2017-12-18 15:22:50 +0100

    Changement Authentification

--- src/main/java/Authentification/auth.java
+++ src/main/java/Authentification/auth.java
@@ -3,6 +3,13 @@ package Authentification;
 import org.gitlab4j.api.GitLabApi;
 
 public class auth {
+	//Retrouve les identifiants des utilisateurs (en attente de comment reprendre les vrai input de l'interface)
+		String identifiant = "professeur";
+		String password = "PCDpotes" ;
+	
 	// Créer une instance pour communiquer avec notre serveur Gitlab
-	GitLabApi gitLabApi = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr","paul.leroux@telecomnancy.eu");
+	//GitLabApi gitLabApi = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr",identifiant) ;
+	
+	// Pour le test d'après
+	GitLabApi gitLabApi = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr", identifiant, password);	
 }
