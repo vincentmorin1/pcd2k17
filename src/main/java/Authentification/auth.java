f6b47e808e73642fa40a1963b52426fb17361d89
Paul Leroux <leroux46u@tncy-04-13.telecomnancy.site.univ-lorraine.fr>
2017-12-19 15:26:49 +0100

    Lecture du fichier où il y a le token

--- src/main/java/Authentification/auth.java
+++ src/main/java/Authentification/auth.java
@@ -1,5 +1,10 @@
 package Authentification;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.StringWriter;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.gitlab4j.api.GitLabApi;
@@ -9,7 +14,7 @@ public class auth{
 	final static Logger log = LogManager.getLogger(auth.class);
 	
 	//Retrouve les identifiants des utilisateurs (en attente de comment reprendre les vrai input de l'interface)
-		private String accessToken = "f1qQVdxzXF8Dn4RmNv3h";
+		private String accessToken = Readfunction();
 		public GitLabApi auth ;
 	// Créer une instance pour communiquer avec notre serveur Gitlab
 	//GitLabApi gitLabApi = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr",identifiant) ;
@@ -18,6 +23,26 @@ public class auth{
 		public auth() {
 			this.auth = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr", accessToken);
 		}
+	private String Readfunction() {
+		    try {
+		       BufferedInputStream lecture = new BufferedInputStream(new FileInputStream("token.txt"));
+		       StringWriter token = new StringWriter();
+		       int b;
+		       while ((b=lecture.read()) != -1)
+		           token.write(b);
+		       token.flush();
+		       token.close();
+		       lecture.close();
+		       return token.toString();
+		    }
+		    catch (IOException ie)
+		    {
+		         ie.printStackTrace(); 
+		    }
+			return null;
+		}
+		
+	
 	public GitLabApi getAuth() {
 		return auth;
 	}
