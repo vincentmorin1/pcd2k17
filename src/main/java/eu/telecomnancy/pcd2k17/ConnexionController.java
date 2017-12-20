6e1ca581b5ddfa1359e382dd38d1292f7487754c
Paul Leroux <leroux46u@tncy-04-13.telecomnancy.site.univ-lorraine.fr>
2017-12-20 09:30:14 +0100

    ecriture du token entrer dans fichier texte token.txt

--- src/main/java/eu/telecomnancy/pcd2k17/ConnexionController.java
+++ src/main/java/eu/telecomnancy/pcd2k17/ConnexionController.java
@@ -1,5 +1,8 @@
 package eu.telecomnancy.pcd2k17;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
@@ -27,6 +30,22 @@ public class ConnexionController{
     log.debug(token.getText());
 	Stage primaryStage = (Stage) connexion.getScene().getWindow();
 	primaryStage.close();
+	
+       final String fichier = "token.txt";
+       try {
+           // creation d'un writer (un écrivain)
+           final FileWriter writer = new FileWriter(fichier);
+           try {
+               writer.write(token.getText());
+           } finally {
+               // quoiqu'il arrive, on ferme le fichier
+               writer.close();
+           }
+       } catch (Exception e) {
+           System.out.println("Bizarre");
+       }
+   
+	
 	
 	Stage stage = new Stage();
 	new HomeView(stage);
