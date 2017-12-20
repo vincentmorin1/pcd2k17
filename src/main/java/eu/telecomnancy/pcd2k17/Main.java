f627da468d9616d3082b2f5c3b0413999acd0c2c
Cedric bell <cedric.bell@telecomnancy.eu>
2017-12-20 19:39:15 +0100

    push de fin de journée : quelques ameliorations base de données

--- src/main/java/eu/telecomnancy/pcd2k17/Main.java
+++ src/main/java/eu/telecomnancy/pcd2k17/Main.java
@@ -16,8 +16,7 @@ public class Main extends Application {
 	
 	//System.out.println(dev.getDevoirId("zaaf") + "/" + proj.getProjetId("zaaf", "gra"));
     log.debug("executing main() method.");
-    launch(args);
-	  
+    launch(args);	  
   }
 
   @Override
