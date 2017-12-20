9cb9d38d86cdd24aa1646599c3e9c82e125f19ff
Cedric bell <cedric.bell@telecomnancy.eu>
2017-12-20 21:43:48 +0100

    daily release

--- src/main/java/Authentification/auth.java
+++ src/main/java/Authentification/auth.java
@@ -50,7 +50,7 @@ public class auth extends maindatabase{
 			}
 
 		}
-		//coucouc vincent
+		//coucouc vincent : en fait non c'est un prank. kdsnùfvkjz^nbjdtbvdfjslm
 	private String Readfunction() {
 		    try {
 		       BufferedInputStream lecture = new BufferedInputStream(new FileInputStream("token.txt"));
